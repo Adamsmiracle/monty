156f0d0ef39297ddb3da1c2a51327a1f6e7112a7
adams miracle <adamsmiracle0@gmail.com>
2023-11-17 01:29:45 +0000

    t

--- pall.c
+++ pall.c
@@ -7,15 +7,15 @@
  */
 void pall(stack_t **head, unsigned int counter)
 {
-    stack_t *h;
-    (void)counter;
+stack_t *h;
+(void)counter;
 
-    h = *head;
-    if (h == NULL)
-        return;
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+h = *head;
+if (h == NULL)
+    return;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
 }
